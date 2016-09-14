0657898dd22ba89cc5ac028f9b2482fe8c536d43
Marcin Gosk <marcin.gosk@gmail.com>
2016-09-14 19:20:39 +0200

    Use time since sumbit instead alive flag

--- status.py
+++ status.py
@@ -8,6 +8,7 @@ logFile = "c:\platform\status.log"
 worker = "worker1"
 wallet = "0xE18c5d3689476fBed966d36c9FEae502FC36d7f2"
 email = "my@email.com"
+secondsSinceSubmitLimit = 480
 
 logging.basicConfig(filename=logFile,level=logging.DEBUG,format='%(asctime)s %(message)s')
 logging.info("Checking status")
@@ -15,14 +16,14 @@ logging.info("Checking status")
 response = urllib2.urlopen("http://dwarfpool.com/eth/api?wallet="+wallet+"&email="+email)
 string = response.read()
 status = json.loads(string)
-alive =  status["workers"][worker]["alive"]
 error = status["error"]
 hashrate = status["workers"][worker]["hashrate"]
 hashrateCalculated = status["workers"][worker]["hashrate_calculated"]
+secondsSinceSubmit = status["workers"][worker]["second_since_submit"]
 
-logging.info("alive:"+ str(alive) + " error:" + str(error) +" HR:"+str(hashrate) +" HRC:"+str(hashrateCalculated))
+logging.info("since submit:"+ str(secondsSinceSubmit) + " error:" + str(error) +" HR:"+str(hashrate) +" HRC:"+str(hashrateCalculated))
 
-if not alive:
+if secondsSinceSubmit > secondsSinceSubmitLimit :
 	logging.error("Restaring miner")
 	os.system("taskkill /F /im ethdcrminer64.exe")
 	os.startfile(claymoreExe)
